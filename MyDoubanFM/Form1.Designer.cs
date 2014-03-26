66f90b35618b51b56f1df8b4d2bef8f85713a104
Wing Gao <wing.gao@live.com>
2014-03-26 14:30:09 +0800

    add files

---
+++ MyDoubanFM/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace MyDoubanFM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowserMain = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxCookie = new System.Windows.Forms.TextBox();
+            this.tabControlMain = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.webBrowserQQ = new System.Windows.Forms.WebBrowser();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxUid = new System.Windows.Forms.TextBox();
+            this.tbxVer = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxMinVer = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabControlMain.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowserMain
+            // 
+            this.webBrowserMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowserMain.Location = new System.Drawing.Point(6, 6);
+            this.webBrowserMain.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowserMain.Name = "webBrowserMain";
+            this.webBrowserMain.ScriptErrorsSuppressed = true;
+            this.webBrowserMain.Size = new System.Drawing.Size(944, 362);
+            this.webBrowserMain.TabIndex = 0;
+            this.webBrowserMain.Url = new System.Uri("http://douban.fm/", System.UriKind.Absolute);
+            this.webBrowserMain.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowserMain_DocumentCompleted);
+             // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(1072, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxCookie
+            // 
+            this.textBoxCookie.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxCookie.Location = new System.Drawing.Point(1057, 256);
+            this.textBoxCookie.Multiline = true;
+            this.textBoxCookie.Name = "textBoxCookie";
+            this.textBoxCookie.Size = new System.Drawing.Size(128, 153);
+            this.textBoxCookie.TabIndex = 2;
+            this.textBoxCookie.Text = "alert(\'hello\')";
+            // 
+            // tabControlMain
+            // 
+            this.tabControlMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControlMain.Controls.Add(this.tabPage1);
+            this.tabControlMain.Controls.Add(this.tabPage2);
+            this.tabControlMain.Location = new System.Drawing.Point(12, 12);
+            this.tabControlMain.Name = "tabControlMain";
+            this.tabControlMain.SelectedIndex = 0;
+            this.tabControlMain.Size = new System.Drawing.Size(969, 400);
+            this.tabControlMain.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.webBrowserMain);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(961, 374);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "DoubanFM";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.webBrowserQQ);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1018, 374);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "QQMusic";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // webBrowserQQ
+            // 
+            this.webBrowserQQ.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowserQQ.Location = new System.Drawing.Point(6, 6);
+            this.webBrowserQQ.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowserQQ.Name = "webBrowserQQ";
+            this.webBrowserQQ.ScriptErrorsSuppressed = true;
+            this.webBrowserQQ.Size = new System.Drawing.Size(1006, 362);
+            this.webBrowserQQ.TabIndex = 0;
+            this.webBrowserQQ.Url = new System.Uri("http://music.qq.com/midportal/musichall/#ctime=1395799690.206", System.UriKind.Absolute);
+            this.webBrowserQQ.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowserQQ_DocumentCompleted);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1008, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ts_uid";
+            // 
+            // tbxUid
+            // 
+            this.tbxUid.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbxUid.Location = new System.Drawing.Point(1085, 40);
+            this.tbxUid.Name = "tbxUid";
+            this.tbxUid.Size = new System.Drawing.Size(100, 21);
+            this.tbxUid.TabIndex = 5;
+            // 
+            // tbxVer
+            // 
+            this.tbxVer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbxVer.Location = new System.Drawing.Point(1085, 67);
+            this.tbxVer.Name = "tbxVer";
+            this.tbxVer.Size = new System.Drawing.Size(100, 21);
+            this.tbxVer.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1008, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "version";
+            // 
+            // tbxMinVer
+            // 
+            this.tbxMinVer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbxMinVer.Location = new System.Drawing.Point(1085, 94);
+            this.tbxMinVer.Name = "tbxMinVer";
+            this.tbxMinVer.Size = new System.Drawing.Size(100, 21);
+            this.tbxMinVer.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1008, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "miniversion";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1197, 421);
+            this.Controls.Add(this.tbxMinVer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbxVer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbxUid);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabControlMain);
+            this.Controls.Add(this.textBoxCookie);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "豆瓣+QQ电台 - by WingGao";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControlMain.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowserMain;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxCookie;
+        private System.Windows.Forms.TabControl tabControlMain;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.WebBrowser webBrowserQQ;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxUid;
+        private System.Windows.Forms.TextBox tbxVer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxMinVer;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MyDoubanFM/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Web;
+using System.Windows.Forms;
+using mshtml;
+using Newtonsoft.Json.Linq;
+
+namespace MyDoubanFM
+{
+    [ComVisible(true)]
+    public partial class Form1 : Form
+    {
+        private const string SosoUrlPre = "http://soso.music.qq.com/fcgi-bin/music3_new.fcg?p=1&catZhida=1&t=100&portal=client&searchid=1828052434720886076&remoteplace=txt.client.top&utf8=1&w=";
+        private bool _isPlayed;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowserMain.ObjectForScripting = this;
+            tbxUid.Text = Properties.Settings.Default.Uid;
+            tbxVer.Text = Properties.Settings.Default.QVer;
+            tbxMinVer.Text = Properties.Settings.Default.QMinVer;
+        }
+
+        private void webBrowserMain_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            HtmlElement head = webBrowserMain.Document.GetElementsByTagName("head")[0];
+            HtmlElement scriptEl = webBrowserMain.Document.CreateElement("script");
+            var element = (IHTMLScriptElement)scriptEl.DomElement;
+            element.text = @"function test(q) {
+        window.external.ParseSoundInfo(JSON.stringify(q))
+    }
+    function myPause(){
+        DBR.act('pause');
+    }
+  
+    function show(x, z, y, s, t, q, w, v, r) {
+        var k = {songName: x, artistName: z || '', channelName: y || '', url: s || '', curl: t || '', type: w || y, coverUrl: q || '', id: v, ssid: r};
+        test(k);
+    }
+    var myInt = setInterval(function () {
+        if (FM) {
+            FM.setCurrentSongInfo = show;
+            clearInterval(myInt);
+        }
+    }, 100);";
+            head.AppendChild(scriptEl);
+        }
+
+        public void ParseSoundInfo(string o)
+        {
+            /*       Root: {
+              "songName": "The Truth That You Leave",
+              "artistName": "Piano Boy",
+              "channelName": "私人兆赫",
+              "url": "http://douban.fm/?start=1812646g317ag0&cid=3812646",
+              "curl": "http://douban.fm/?cid=0",
+              "type": "私人兆赫",
+              "coverUrl": "http://img5.douban.com/lpic/s4435637.jpg",
+              "id": "1812646",
+              "ssid": "317a"}  */
+            webBrowserMain.Document.InvokeScript("myPause");
+            JObject jo = JObject.Parse(o);
+            string searchurl = SosoUrlPre + HttpUtility.UrlEncode(jo["songName"] + " " + jo["artistName"]);
+            SetQqMusicCookie(searchurl);
+            webBrowserQQ.Navigate(searchurl);
+            _isPlayed = false;
+        }
+
+        private void webBrowserQQ_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            string url = webBrowserQQ.Url.ToString();
+            if (url.StartsWith("http://soso.music.qq.com"))
+            {
+                HtmlElement head = webBrowserQQ.Document.GetElementsByTagName("head")[0];
+                HtmlElement scriptEl = webBrowserQQ.Document.CreateElement("script");
+                var element = (IHTMLScriptElement)scriptEl.DomElement;
+                element.text = @"function myPlay(){g_player.play(document.getElementsByClassName('btn_play')[0])}";
+                head.AppendChild(scriptEl);
+                if (!_isPlayed)
+                {
+                    webBrowserQQ.Document.InvokeScript("myPlay");
+                    _isPlayed = true;
+                    webBrowserMain.Document.InvokeScript("myPause");
+                }
+            }
+        }
+
+        [DllImport("wininet.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool InternetSetCookie(string lpszUrlName, string lbszCookieName, string lpszCookieData);
+
+        /// <summary>
+        /// 设置QQ搜索的必要cookie
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        private string SetQqMusicCookie(string url)
+        {
+            string cookie = textBoxCookie.Text;
+            InternetSetCookie(url, "ts_uid", tbxUid.Text);
+            InternetSetCookie(url, "qqmusic_version", tbxVer.Text);
+            InternetSetCookie(url, "qqmusic_miniversion", tbxMinVer.Text);
+            SaveSettings();
+            return cookie;
+        }
+
+        private void SaveSettings()
+        {
+            Properties.Settings.Default.Uid = tbxUid.Text;
+            Properties.Settings.Default.QVer = tbxVer.Text;
+            Properties.Settings.Default.QMinVer = tbxMinVer.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+       
+
+
+
+
+
+        
+    }
+}
